d8c2751d1c7fa975421ab496deed902af601badd
Administrator <Administrator@WIN-C5D49FN17LD>
2022-09-09 12:31:16 +0200

    Add project files.

---
+++ Tur and Retur Kørselslogbog/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Tur_and_Retur_Kørselslogbog
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=WIN-C5D49FN17LD;Initial Catalog=Registeration;Integrated Security=True");
+        //SqlConnection cmd = new SqlConnection();
+        //SqlConnection da = new SqlConnection();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+                if (txtUsername.Text == "" && txtpassword.Text == "" && txtComPassword.Text == "")
+                {
+                    MessageBox.Show("Username and Password fields are empty", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (txtpassword.Text == txtComPassword.Text)
+                {
+                conn.Open();
+                string register = "INSERT INTO tblUser (username, password) VALUES ('" + txtUsername.Text + "', '" + txtpassword.Text + "')";
+                SqlConnection cmd = new SqlConnection(register);
+                conn.Close();
+
+            }
+
+          
+
+        }
+    }
+}
---
+++ Tur and Retur Kørselslogbog/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tur and Retur Kørselslogbog")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TEC")]
+[assembly: AssemblyProduct("Tur and Retur Kørselslogbog")]
+[assembly: AssemblyCopyright("Copyright © TEC 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b878fb82-16ed-4df5-8e8c-e9460479678e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
